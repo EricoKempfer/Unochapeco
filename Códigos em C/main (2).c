bd761899ec1d69ca5f847f860e490f09cc3d07f0
Érico Kempfer <165982225+EricoKempfer@users.noreply.github.com>
2024-10-20 14:34:32 -0300

    Sort Ordenação

---
+++ Códigos em C/main (2).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int v[5], i, numero,numeroatras;
+    for(i=0;i<5;i++){
+        scanf("%d", &v[i]);
+        
+    }
+    
+    for(i=1;i<5;i++){
+        numero = v[i];
+        numeroatras = i-1;
+        while(numeroatras>=0 && v[numeroatras]>numero){
+            v[numeroatras+1]=v[numeroatras];
+            numeroatras=numeroatras-1;
+        }
+        v[numeroatras+1] = numero;
+    }
+    for(i=0;i<5;i++){
+        printf("%d\n", v[i]);
+    }
+   
+    
+
+    return 0;
+}
